e3c066860663a363e4841dcf308f154872e912cc
ahtoh <472668561@qq.com>
2021-11-26 10:49:27 +0800

    first commit

---
+++ hello_world.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ahtoh
+ * @Date: 2021-11-26 10:38:12
+ * @LastEditors: ahtoh
+ * @LastEditTime: 2021-11-26 10:40:59
+ */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
