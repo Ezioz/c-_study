2418b44f3a39f1f9ee0cea51121fd9ab22666c11
ahtoh <472668561@qq.com>
2021-11-26 10:51:11 +0800

    first commit

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,15 +1,15 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: ahtoh
  * @Date: 2021-11-26 10:38:12
  * @LastEditors: ahtoh
- * @LastEditTime: 2021-11-26 10:40:59
+ * @LastEditTime: 2021-11-26 10:51:01
  */
 
-#include <iostream>
-using namespace std;
-int main()
+#include <iostream>  // 头文件
+using namespace std; // 命名空间
+int main()           // main仍然是程序开头
 {
     cout << "hello world!" << endl;
     return 0;
